81ce1b661dadfc8e2800c1e8e9bad1f5ad3dbb26
liningning <884558315@qq.com>
2017-02-20 16:29:20 +0800

    增加了 toIpAddr()方法来返回ip地址

--- springWeb/src/com/lanyuan/util/Common.java
+++ springWeb/src/com/lanyuan/util/Common.java
@@ -1,5 +1,7 @@
 package com.lanyuan.util;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class Common {
 
 	public static boolean isEmpty(String s)
@@ -12,6 +14,26 @@ public class Common {
 		{
 			return false;
 		}
+		
+	}
+	
+	//返回用户的ip地址
+	public static String toIpAddr(HttpServletRequest request)
+	{
+		String ip =request.getHeader("x-forwarded-for");
+		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 		
 	}
 }
